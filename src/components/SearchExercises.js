df72f32bb23410c2f13f5a81d1362330a1c33198
haddedseif746@gmail.com <haddedseif746@gmail.com>
2024-02-24 02:36:03 +0100

    first commit

---
+++ src/components/SearchExercises.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import { Box, Button, Stack, TextField, Typography } from "@mui/material";
+const SearchExercises = () => {
+  return (
+    <Stack alignItems="center" mt="37px" justifyContent="center">
+      <Typography
+        fontWeight={700}
+        sx={{ fontSize: { lg: "44px", xs: "30px" } }}
+        mb="50px"
+        textAlign="center"
+      >
+        Awesome Exercises You
+        <br />
+        Should Know
+      </Typography>
+      <Box position="relative" mb="72px">
+        <TextField
+          sx={{
+            input: {
+            fontWeight: "700",
+            border: "none",
+            borderRadius: "4px" 
+          },
+          width:{lg: "800px", xs: "350px"},
+          backgroundColor:'#fff',borderRadius:'40px'
+          }}
+          height="76px"
+          value=""
+          onChange={(e) => {}}
+          placeholder="Search  Exercises"
+          type="text"
+        />
+        <Button className="search-btn" sx={{bgcolor:'#FF2625',color:'#fff',textTransform:'none',width:{lg:'175px', xs:'80px'},fontSize:{lg:'20px', xs:'14px'},height:'56px',position:"absolute"}}>Search</Button>
+      </Box>
+    </Stack>
+  );
+};
+
+export default SearchExercises;
