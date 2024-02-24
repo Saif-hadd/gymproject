b769a8a1b79e293c891926554478fb8550b3eb79
haddedseif746@gmail.com <haddedseif746@gmail.com>
2024-02-24 19:38:49 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import React from "react";
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Box } from '@mui/material';
+
 import './App.css';
-import {Route, Routes} from 'react-router-dom';
-import {Box} from'@mui/material';
-import ExerciseDetail from "./pages/ExerciseDetail";
-import Home from "./pages/Home";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-const App = () => {
-  return( 
-    <Box width="400px" sx={{ width: {xl: '1488px'}}} m="auto">
-    <Navbar/>
+import ExerciseDetail from './pages/ExerciseDetail';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
+const App = () => (
+  <Box width="400px" sx={{ width: { xl: '1488px' } }} m="auto">
+    <Navbar />
     <Routes>
-    <Route path="/"element= {<Home/>}/>
-    <Route path="/exercise/:id"element= {<ExerciseDetail/>}/>
+      <Route path="/" element={<Home />} />
+      <Route path="/exercise/:id" element={<ExerciseDetail />} />
     </Routes>
-    <Footer/>
-    </Box>
-    )
-    
-}
+    <Footer />
+  </Box>
+);
 
 export default App;
-
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import ReactDOM  from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+
 import App from './App';
-import { BrowserRouter } from 'react-router-dom'
-const root =ReactDOM.createRoot(document.getElementById('root'));
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
+  <React.StrictMode>
     <BrowserRouter>
-    <App/>
+      <App />
     </BrowserRouter>
-)
+  </React.StrictMode>,
+);
